317ca9e929b40d86e1e8edf0659aff0581a2375b
Muhammad Qaiser Jamil <qaiser.jamil@live.com.au>
2021-08-11 21:01:57 +1000

    Add files via upload

---
+++ environment/string-data-type.py
@@ -0,0 +1,16 @@
+myString="This is a string."
+print(myString)
+print(type(myString))
+print(str(myString) + "is of data type: " + str(type(myString)))
+
+firstString="Water"
+secondString="Fall"
+thirdString= firstString+secondString
+print(thirdString)
+name=input("What is your name? ")
+print("Your name is "+name+".")
+color=input("What is your favourite color? ")
+animal=input ("What is your favourite animal? ")
+print("{}, you like a {} {}!" .format(name,color,animal))
+
+
